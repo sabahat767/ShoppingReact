555205a5631140a097c69409f65ab693273f9ecd
sabahat767 <61451012+sabahat767@users.noreply.github.com>
2020-11-23 12:50:24 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Header from './components/header'
+import Body from './components/body'
+import './App.css';
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+<Header/>
+<Body/>
+      </div>
+    )
+  }
+}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+        
+        
+//         <a>
+          
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
